91ef41434c36f2af82f608217bc2fbd22f80ee76
zakharyans-cmd <zakharyans@yandex.ru>
2026-04-22 16:28:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import os
-from telegram import Update
+from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
 
+# кнопки
+keyboard = [
+    ["📩 Оставить заявку"],
+    ["ℹ️ Услуги"]
+]
+
+reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🚀 Бот работает 24/7")
+    await update.message.reply_text(
+        "👋 Добро пожаловать!\n\n"
+        "🤖 Я бот для бизнеса, который:\n"
+        "• принимает заявки 24/7\n"
+        "• отвечает клиентам автоматически\n"
+        "• упрощает запись на услуги\n\n"
+        "👇 Попробуйте, как это работает:",
+        reply_markup=reply_markup
+    )
+
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Напиши /start")
+
 
 app = ApplicationBuilder().token(TOKEN).build()
 
